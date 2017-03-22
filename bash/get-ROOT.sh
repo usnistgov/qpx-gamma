af6703520be4b3d72b52894e43825ab6d502b0da
MGS <martukas@gmail.com>
2017-03-22 18:16:51 +0100

    root prereqs installation more complete

--- bash/get-ROOT.sh
+++ bash/get-ROOT.sh
@@ -8,7 +8,14 @@ fi
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
-sudo apt-get --yes install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev
+sudo apt-get --yes install git dpkg-dev cmake \
+g++ gcc binutils libx11-dev libxpm-dev libxft-dev \
+libxext-dev gfortran libssl-dev libpcre3-dev \
+xlibmesa-glu-dev libglew1.5-dev libftgl-dev \
+libmysqlclient-dev libfftw3-dev libcfitsio-dev \
+graphviz-dev libavahi-compat-libdnssd-dev \
+libldap2-dev python-dev libxml2-dev libkrb5-dev \
+libgsl0-dev libqt4-dev
 
 git clone http://root.cern.ch/git/root.git
 cd root
