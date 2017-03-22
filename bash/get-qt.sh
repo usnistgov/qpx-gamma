e384c2b6707cb12ed1a88269c5f5aab242d19f4e
MGS <martukas@gmail.com>
2017-03-22 17:51:16 +0100

    get-qt.sh now modifies .profile

--- bash/get-qt.sh
+++ bash/get-qt.sh
@@ -23,11 +23,20 @@ if [ `getconf LONG_BIT` = "64" ]
 then
   wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
   chmod +x qt-unified-linux-x64-online.run
+  echo Will now install Qt using the online installer. Make sure to chose Qt5.5.
+  read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
   ./qt-unified-linux-x64-online.run
-  rm qt-unified-linux-x64-online.run    
+  rm qt-unified-linux-x64-online.run
+  LINE="export CMAKE_PREFIX_PATH=$HOME/Qt/5.5/gcc_64"
 else
   wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x86-online.run
   chmod +x qt-unified-linux-x86-online.run
+  echo Will now install Qt using the online installer. Make sure to chose Qt5.5.
+  read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
   ./qt-unified-linux-x86-online.run
   rm qt-unified-linux-x86-online.run    
+  LINE="export CMAKE_PREFIX_PATH=$HOME/Qt/5.5/gcc"
 fi
+
+eval $LINE
+printf '%s\n' "$LINE" >> ~/.profile
